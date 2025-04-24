c6bfde1f7e6b816fa6359a5ae9d4c3378126fa1a
Diego Carvalho <157425806+diegocrvo@users.noreply.github.com>
2025-04-24 00:40:02 -0300

    feat: add exercises

---
+++ debugging-java/src/br/com/dio/debugging/Main.java
@@ -0,0 +1,30 @@
+package br.com.dio.debugging;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Início do programa no método main.");
+        a();
+        System.out.println("Finalizou o programa no método main.");
+    }
+
+    static void a() {
+        System.out.println("Entrou no método a.");
+        b();
+        System.out.println("Finaliou o método a.");
+    }
+
+    static void b() {
+        System.out.println("Entrou no método b.");
+        for (int i = 0; i <= 4; i++) System.out.println(i);
+        c();
+        System.out.println("Finalizou o método b.");
+    }
+
+    static void c() {
+        System.out.println("Entrou no método c.");
+        Thread.dumpStack();
+        System.out.println("Finalizou o método c.");
+    }
+}
